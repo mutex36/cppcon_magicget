21eade29c17ef9f812cb6713dec3a5f91efb07c8
Luis von der Eltz <luis9.6@hotmail.de>
2018-01-03 02:21:33 +0100

    Update main.cpp

--- sfinae/main.cpp
+++ sfinae/main.cpp
@@ -1,142 +1,108 @@
-//
-//  main.cpp
-//  sfinae
-//
-//  Created by Luis von der Eltz on 13.05.17.
-//  Copyright © 2017 Luis von der Eltz. All rights reserved.
-//
-
-#include <iostream>
-
-#include <string>
-#include <type_traits>
-#include <array>
-
-namespace Serialization
+namespace Core::Reflection
 {
-    namespace Detail
-    {
-        
-#define CR_SERIALIZATION_MAX_FIELDS 10
-#define CR_SERIALIZATION_TYPE(TYPE, ID)          \
-template<>                                       \
-constexpr size_t type_id<TYPE>() noexcept        \
-{                                                \
-    return ID;                                   \
-}                                                \
-    
-        template<typename T>
-        constexpr size_t type_id() noexcept
-        {
-            return 0;
-        }
-        CR_SERIALIZATION_TYPE(char, 1);
-        CR_SERIALIZATION_TYPE(unsigned char, 2);
-        CR_SERIALIZATION_TYPE(int, 3);
-        CR_SERIALIZATION_TYPE(unsigned int, 4);
-        CR_SERIALIZATION_TYPE(long long, 5);
-        CR_SERIALIZATION_TYPE(unsigned long long, 6);
-        CR_SERIALIZATION_TYPE(float, 7);
-        CR_SERIALIZATION_TYPE(double, 8);
-        CR_SERIALIZATION_TYPE(size_t, 9);
-        
-        struct type
-        {
-            size_t hash;
-            size_t index;
-            size_t size;
-        };
-        
-        template<size_t I>
-        struct ubiq
-        {
-            template<typename T>
-            constexpr operator T() const noexcept
-            {
-                return T{};
-            }
-        };
-        
-        template<size_t I>
-        struct ubiq_val
-        {
-            type* type_list;
-            
-            template<typename T, typename D = typename std::decay<T>::type>
-            constexpr operator T() const noexcept
-            {
-                auto& tp = type_list[I];
-                tp.hash = type_id<D>();
-                tp.index = I;
-                tp.size = sizeof(T);
-                
-                return T{};
-            }
-        };
-        
-        template<typename T, size_t I0, size_t...I>
-        constexpr auto count_fields(size_t& out, std::index_sequence<I0, I...>)
-            -> decltype(T{ ubiq<I0>{}, ubiq<I>{}... })
-        {
-            out = sizeof...(I) + 1;
-            return {};
-        }
-        
-        template<typename T, size_t...I>
-        constexpr void count_fields(size_t& out, std::index_sequence<I...>)
-        {
-            count_fields<T>(out, std::make_index_sequence<sizeof...(I) - 1>{});
-        }
-        
-        template<typename T, size_t...N>
-        constexpr auto get_types(std::array<type, sizeof...(N)>& out, std::index_sequence<N...>)
-            -> decltype(T{ ubiq<N>{}... })
-        {
-            T temp{ubiq_val<N>{out.data()}...};
-            return temp;
-        }
-    }
- 
-    template<typename T>
-    constexpr size_t GetFieldCount()
-    {
-        size_t r{0};
-        Detail::count_fields<T>(r, std::make_index_sequence<CR_SERIALIZATION_MAX_FIELDS>{});
-        return r;
-    }
-    
-    template<typename T>
-    constexpr auto GetTypes()
-    {
-        std::array<Detail::type, GetFieldCount<T>()> types;
-        Detail::get_types<T>(types, std::make_index_sequence<types.size()>{});
-        return types;
-    }
-}
+	namespace _Detail
+	{
+		template<typename T>
+		struct type_to_hash;
 
-struct test
-{
-    size_t a;
-    float b;
-    unsigned int ff;
-    double g;
-    unsigned int* dddf;
-};
+		template<>
+		struct type_to_hash<int> {
+			static constexpr size_t hash = 1;
+		};
 
-int main(int argc, const char * argv[])
-{
+		template<>
+		struct type_to_hash<float> {
+			static constexpr size_t hash = 2;
+		};
 
-    std::cout << "Struct has " << Serialization::GetFieldCount<test>() << " fields." << std::endl;
+		template<>
+		struct type_to_hash<double> {
+			static constexpr size_t hash = 3;
+		};
 
-    for (auto& type : Serialization::GetTypes<test>())
-    {
-        std::cout << "Field {";
-        std::cout << "Type: " << type.name;
-        std::cout << ", Size: " << type.size;
-        std::cout << ", Index: " << type.index;
-        std::cout << ", Hash: " << type.hash;
-        std::cout << "}" << std::endl;
-    }
-    
-    return 0;
+		template<size_t I>
+		struct hash_to_type;
+
+		template<>
+		struct hash_to_type<1> {
+			typedef int type;
+		};
+
+		template<>
+		struct hash_to_type<2> {
+			typedef float type;
+		};
+
+		template<>
+		struct hash_to_type<3> {
+			typedef double type;
+		};
+
+		template<size_t I>
+		struct ubiq {
+			template<typename T>
+			constexpr operator T() const noexcept {
+				return T{};
+			}
+		};
+
+		template<size_t I>
+		struct ubiq_val {
+			size_t& _hash;
+
+			template<typename T>
+			constexpr operator T() const noexcept {
+				_hash = type_to_hash<std::decay_t<T>>::hash;
+				return T{};
+			}
+		};
+
+		template<typename T, size_t I0, size_t...I>
+		constexpr auto count_fields_impl(size_t& out, std::index_sequence<I0, I...>)
+			-> decltype(T{ ubiq<I0>{}, ubiq<I>{}... }) {
+			out = sizeof...(I)+1;
+			return {};
+		}
+
+		template<typename T, size_t...I>
+		constexpr void count_fields_impl(size_t& out, std::index_sequence<I...>) {
+			count_fields_impl<T>(out, std::make_index_sequence<sizeof...(I)-1>{});
+		}
+
+		template<typename T>
+		constexpr size_t count_fields() {
+			size_t out = 0;
+			count_fields_impl<T>(out, std::make_index_sequence<20>());
+			return out;
+		}
+
+		template<typename T, size_t...N, size_t N0>
+		constexpr auto get_field_hash_impl(size_t& out, std::index_sequence<N0, N...>) {
+			T temp{ ubiq<N>{}..., ubiq_val<N0>{out} };
+			return temp;
+		}
+
+		template<typename T, size_t Index>
+		constexpr size_t get_field_hash() {
+			size_t out = 0;
+			get_field_hash_impl<T>(out, std::make_index_sequence<Index + 1>{});
+			return out;
+		}
+
+		template<typename T, size_t Index>
+		struct field_type {
+			using type = typename _Detail::hash_to_type<
+				_Detail::get_field_hash<T, Index>()>::type;
+		};
+	}
+
+	template<typename T>
+	struct Reflect {
+		static_assert(std::is_class<T>::value, "reflected type is not a struct/class");
+
+		static constexpr size_t field_count = _Detail::count_fields<T>();
+
+		template<size_t Index>
+		using field_type = typename _Detail::field_type<T, Index>::type;
+	};
 }
